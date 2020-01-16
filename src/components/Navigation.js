6ee9e091c21eaac50fdbcb5db72fc8882f2bed03
unknown <fnumilat@gmail.com>
2020-01-15 18:34:37 -0600

    work done

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navigation = () => {
   return (
@@ -6,13 +7,13 @@ const Navigation = () => {
       <div className="App">
         <h1>React Router Mini</h1>
         <div>
-          <a href="">Home</a>
+          <a><Link to="/">Home</Link></a>
         </div>
         <div>
-          <a href="">About</a>
+          <a><Link to="/about">About</Link></a>
         </div>
         <div>
-          <a href="">Contact</a>
+          <a><Link to="/contact">Contact</Link></a>
         </div>
       </div>
     </div>
